d98b8fa58f431b7124f21d9464fd2cf0b8663502
Hubert <szpiners@gmail.com>
2025-03-20 23:25:31 +0100

    database

--- BloodMarket/settings.py
+++ BloodMarket/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'BloodMarket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blood_market',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
