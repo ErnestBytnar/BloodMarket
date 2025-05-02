80bf1ba0c5e2b3682e246d6b38aa1f335a6f169d
Hubert <szpiners@gmail.com>
2025-05-03 01:11:39 +0200

    Przywrócenie navbara oraz możliwości wylogowania

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,20 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import HomePage from "./pages/HomePage/HomePage";
 import AddProductPage from "./pages/AddProductPage/AddProductPage"
+import Layout from "./components/Layout";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
     return (
         <AuthProvider>
             <Router>
                 <Routes>
+                <Route path="/" element={<Layout />}  >
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/register" element={<RegisterPage />} />
                     <Route path="/dashboard" element={<HomePage />} />
                     <Route path="/add" element={<AddProductPage />} />
-
+                    </Route>
                 </Routes>
             </Router>
         </AuthProvider>
